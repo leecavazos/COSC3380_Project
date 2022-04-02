30b81fdbe7bf61eac3f81bb7256df05f0970ddb8
Minh Nguyen <vn.ndminh@gmail.com>
2022-04-02 15:37:01 -0500

    Add toggle password to seperate js file for further use

---
+++ POS/js/script.js
@@ -0,0 +1,12 @@
+// Toggle the visibility of password
+const togglePassword = document.querySelector("#togglePassword");
+    const password = document.querySelector("#passInput");
+
+    togglePassword.addEventListener("click", function () {
+        // toggle the type attribute
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        
+        // toggle the icon
+        this.classList.toggle("bi-eye");
+});
