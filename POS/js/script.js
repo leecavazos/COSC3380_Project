57588d14f2b4e1ba3c1ca63c8ada97b59f8431d3
Minh Nguyen <vn.ndminh@gmail.com>
2022-04-05 14:57:27 -0500

    Wrap the toggle password into a function

--- POS/js/script.js
+++ POS/js/script.js
@@ -1,12 +1,10 @@
 // Toggle the visibility of password
-const togglePassword = document.querySelector("#togglePassword");
-const password = document.querySelector("#passInput");
-
-togglePassword.addEventListener("click", function () {
+function togglePassword() {
+    const togglePassword = document.querySelector("#togglePassword");
+    const password = document.querySelector("#passInput"); 
     // toggle the type attribute
     const type = password.getAttribute("type") === "password" ? "text" : "password";
     password.setAttribute("type", type);
-    
     // toggle the icon
-    this.classList.toggle("bi-eye");
-});
+    togglePassword.classList.toggle("bi-eye");
+};
