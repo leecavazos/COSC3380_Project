6df64d9a5f27869a60a8827a7a708882eb0b1dd6
leecavazos <cavazoslee@gmail.com>
2022-04-24 16:10:54 -0500

    Fixing issue with Pie Chart

--- POS/pages/OrdersManager/reportlayout.php
+++ POS/pages/OrdersManager/reportlayout.php
@@ -94,6 +94,7 @@
             <?php
                 $sql = "SELECT Product_ID, Sum(Quantity) FROM POS.`Line Item` group by Product_ID";
                 $sql2 = "SELECT COUNT(MONTH(Date_of_purchase)), MONTHNAME(Date_of_purchase) FROM POS.`Order` group by MONTHNAME(Date_of_purchase)";
+                
                 $result = mysqli_query($conn, $sql);
                 $result9 = mysqli_query($conn, $sql2);
                 $sampleArrayID = array();
