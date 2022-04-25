1e071e9383029be75d9d6caa47bb72bed182f676
Minh Nguyen <vn.ndminh@gmail.com>
2022-04-24 21:12:07 -0500

    Change to the new database

--- POS/php/config.php
+++ POS/php/config.php
@@ -2,7 +2,7 @@
     define('DB_SERVER', '18.116.44.118');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'cosc3380');
-    define('DB_DATABASE', 'POS');
+    define('DB_DATABASE', 'newPOS');
     $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
